a0869247f1c89c4790b2ce74bca5f3fbefff7928
KatyWittwer <katy.k.wittwer@gmail.com>
2017-11-22 01:00:43 -0600

    initial commit

---
+++ src/main/java/org/launchcode/attendtrac/AttendtracApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.attendtrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendtracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendtracApplication.class, args);
+	}
+}
