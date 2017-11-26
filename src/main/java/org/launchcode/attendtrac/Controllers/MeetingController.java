41e59a27f5c6258af17f4bac98067ef159c08075
KatyWittwer <katy.k.wittwer@gmail.com>
2017-11-26 10:40:22 -0600

    removed add.html and updated meeting controller with view

--- src/main/java/org/launchcode/attendtrac/Controllers/MeetingController.java
+++ src/main/java/org/launchcode/attendtrac/Controllers/MeetingController.java
@@ -1,8 +1,27 @@
 package org.launchcode.attendtrac.Controllers;
-
+import org.launchcode.attendtrac.Models.data.MeetingDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @Controller
+@RequestMapping("/meeting")
 public class MeetingController {
-    
+
+    @Autowired
+    private MeetingDao meetingDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("meetings", meetingDao.findAll());
+        model.addAttribute("title", "All Meetings");
+
+        return "meeting/index";
+
+    }
 }
+
