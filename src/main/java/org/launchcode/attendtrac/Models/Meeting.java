78f575fce0c48d025d252d6ee50cf3877257ef6e
KatyWittwer <katy.k.wittwer@gmail.com>
2017-12-02 17:17:16 -0600

    added many to many relationship with attendee

--- src/main/java/org/launchcode/attendtrac/Models/Meeting.java
+++ src/main/java/org/launchcode/attendtrac/Models/Meeting.java
@@ -1,12 +1,15 @@
 package org.launchcode.attendtrac.Models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javafx.util.converter.LocalDateStringConverter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Meeting {
@@ -16,14 +19,16 @@ public class Meeting {
     private int id;
 
     @NotNull
-    @Size(min = 1, max = 10, message = "Date must not be empty")
-    private Date date;
-
-    @NotNull
     @Size(min = 1, max = 15, message = "Meeting type must not be empty")
     private String type;
 
-    public Meeting(int id, String type, Date date) {
+    @NotNull
+    private String date;
+
+    @ManyToMany
+    private List<Attendee> attendees;
+
+    public Meeting(int id, String type, String date) {
         this.id = id;
         this.type = type;
         this.date = date;
@@ -43,11 +48,11 @@ public class Meeting {
         this.type = type;
     }
 
-    public Date getDate() {
+    public String getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(String date) {
         this.date = date;
     }
 
