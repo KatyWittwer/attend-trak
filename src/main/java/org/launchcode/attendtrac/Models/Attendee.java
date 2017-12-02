525aaa8e8506fae82ab28ecab5508b497900a956
KatyWittwer <katy.k.wittwer@gmail.com>
2017-12-02 17:17:42 -0600

    added many to many relationship with meeting

--- src/main/java/org/launchcode/attendtrac/Models/Attendee.java
+++ src/main/java/org/launchcode/attendtrac/Models/Attendee.java
@@ -1,4 +1,42 @@
 package org.launchcode.attendtrac.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
 public class Attendee {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String name;
+
+    @ManyToMany(mappedBy = "meetings")
+    private List<Meeting> meetings;
+
+    public Attendee(String name) {
+        this.name = name;
+    }
+
+    public Attendee() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
