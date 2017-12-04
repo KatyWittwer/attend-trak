600ef9817e1ca613ac5d12860590176c2bdd528d
KatyWittwer <katy.k.wittwer@gmail.com>
2017-12-03 23:24:41 -0600

    added routing for add page

--- src/main/java/org/launchcode/attendtrac/Controllers/UserController.java
+++ src/main/java/org/launchcode/attendtrac/Controllers/UserController.java
@@ -1,4 +1,52 @@
 package org.launchcode.attendtrac.Controllers;
 
+import org.launchcode.attendtrac.Models.Forms.User;
+import org.launchcode.attendtrac.Models.data.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("user")
 public class UserController {
+
+    @Autowired
+    private UserDao userDao;
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("title","Register");
+        model.addAttribute("user", new User());
+
+        model.addAttribute("users", userDao.findAll());
+
+        return "user/add";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String displayAddUserForm(@ModelAttribute @Valid User user, Errors errors,
+                                     String verify, Model model) {
+
+        model.addAttribute(new User());
+        model.addAttribute("user", user);
+
+        boolean passwordsMatch = true;
+        if ((user.getPassword() == null) || verify == null
+                || !user.getPassword().equals(verify)) {
+            passwordsMatch = false;
+            model.addAttribute("verifyError", "Password must match");
+        }
+
+        if (passwordsMatch) {
+            return "user/index";
+        }
+
+        return "user/add";
+
+    }
 }
