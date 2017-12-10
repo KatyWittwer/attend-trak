93801213a508fef68e6c66604dd5a30a2f6adddd
KatyWittwer <katy.k.wittwer@gmail.com>
2017-12-09 20:16:54 -0600

    changed so the meeting is created first them attendees added

--- src/main/java/org/launchcode/attendtrac/Controllers/MeetingController.java
+++ src/main/java/org/launchcode/attendtrac/Controllers/MeetingController.java
@@ -1,14 +1,13 @@
 package org.launchcode.attendtrac.Controllers;
 import org.launchcode.attendtrac.Models.Attendee;
+import org.launchcode.attendtrac.Models.Forms.AddMeetingAttendee;
 import org.launchcode.attendtrac.Models.Meeting;
 import org.launchcode.attendtrac.Models.data.AttendeeDao;
 import org.launchcode.attendtrac.Models.data.MeetingDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
@@ -23,9 +22,8 @@ public class MeetingController {
     @Autowired
     private AttendeeDao attendeeDao;
 
-    @RequestMapping(value = "")
+    @RequestMapping(value = "index")
     public String index(Model model) {
-
         model.addAttribute("title", "Board Meetings");
         model.addAttribute("meetings", meetingDao.findAll());
 
@@ -34,28 +32,64 @@ public class MeetingController {
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddMeetingForm(Model model) {
-        model.addAttribute("title", "Enter Meeting Attendance Record");
+        model.addAttribute("title", "Enter New Meeting:");
         model.addAttribute(new Meeting());
-
-        model.addAttribute("attendees", attendeeDao.findAll());
-
         return "meeting/add";
 
     }
+
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddMeetingForm(@ModelAttribute @Valid Meeting newMeeting,
-                                        Errors errors, Model model) {
+    public String processAddMeetingForm(Model model, @ModelAttribute @Valid Meeting meeting,
+                                        Errors errors) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Meeting");
             return "meeting/add";
         }
 
-        meetingDao.save(newMeeting);
-        return "redirect/view/";
+        meetingDao.save(meeting);
+        return "redirect:attendance/" + meeting.getId();
 
+    }
+
+    @RequestMapping(value = "attendance/{meetingId}", method = RequestMethod.GET)
+    public String addMeetingAttendee(Model model, @PathVariable int meetingId) {
+
+        Meeting meeting = meetingDao.findOne(meetingId);
+
+        AddMeetingAttendee form = new AddMeetingAttendee(
+                attendeeDao.findAll(), meeting);
+
+        model.addAttribute("title", "Record Attendees");
+        model.addAttribute("form", form);
+        return "meeting/attendance";
+    }
+
+    @RequestMapping(value = "attendance", method = RequestMethod.POST)
+    public String addMeetingAttendee(Model model, @ModelAttribute @Valid AddMeetingAttendee form, Errors errors){
+
+        if (errors.hasErrors()) {
+            model.addAttribute("form", form);
+            return "menu/attendance";
         }
 
+        Attendee theAttendee = attendeeDao.findOne(form.getAttendeeId());
+        Meeting theMeeting = meetingDao.findOne(form.getMeetingId());
+        theMeeting.addMeetingAttendee(theAttendee);
+
+        meetingDao.save(theMeeting);
+        return "redirect:/meeting/index";
 
 }
+    @RequestMapping(value = "view/{meetingId}", method = RequestMethod.GET)
+    public String viewMeeting(Model model, @PathVariable int meetingId) {
 
+        Meeting meeting = meetingDao.findOne(meetingId);
+        model.addAttribute("attendees", attendeeDao.findAll());
+
+        model.addAttribute("title", meeting.getDate());
+        model.addAttribute("meetingId", meeting.getId());
+
+        return "meeting/{view}";
+    }
+}
